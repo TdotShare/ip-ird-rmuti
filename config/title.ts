ef14b3b1253905b92f0dbcb6462648bbd456f4c5
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2022-04-13 15:55:43 +0700

    update theme
    update theme

---
+++ config/title.ts
@@ -0,0 +1,6 @@
+export const titleConfig  =  {
+    NameWeb : `งานทรัพย์สินทางปัญญา - สวพ.มทร.อีสาน`,
+    NameFull : `งานทรัพย์สินทางปัญญา`,
+    OrganizationName : `สวพ.`,
+    RmutiName : `มทร.อีสาน`,
+}
